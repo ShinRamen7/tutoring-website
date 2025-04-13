41bf2606f353bd421cea3ceecc5c368d0e20f3db
Jasper Shin <hwshin@Jaspers-MacBook-Air-926.local>
2025-04-13 15:35:46 -0400

    Save before pull

--- models/Tutor.js
+++ models/Tutor.js
@@ -13,3 +13,15 @@ const tutorSchema = new mongoose.Schema({
 
 module.exports = mongoose.model('Tutor', tutorSchema);
 
+const tutorSchema = new mongoose.Schema({
+    name: String,
+    university: String,
+    classYear: String,
+    bio: String,
+    longBio: String,
+    resume: String,
+    photoUrl: String,
+    specialties: [String],
+    acceptedSchools: [String]
+});
+
